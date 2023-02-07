91665511680ac6d2c05509cb6346f643d2ca1ab9
gkasianenko <g.kasianenko@yandex.ru>
2023-02-08 01:38:59 +0300

    global styling

--- src/App.js
+++ src/App.js
@@ -10,22 +10,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div class="wrapper">
-        <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
-              <Product/>
-              <Product/>
-              <Product/>
-              <Product/>
-              <Product/>   
-            </div>
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
           </div>
         </div>
       </div>
